0f6d621972172089d59ef56713a40a368ae11065
frontjss <frontjss@gmail.com>
2017-06-28 19:41:24 +0300

    worked

--- telebot/bot.py
+++ telebot/bot.py
@@ -8,16 +8,6 @@ import json
 import cherrypy
 import config
 
-WEBHOOK_HOST = '194.67.217.180'
-WEBHOOK_PORT = 80  # 443, 80, 88 или 8443 (порт должен быть открыт!)
-WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
-
-WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
-WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
-
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (config.token)
-
 bot = telebot.TeleBot(config.token)
 
 # a = {"id":"0","name":"durak"}
