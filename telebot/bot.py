ed9b51b721e932f1cf9344a7bdadbe2aadbcaae6
frontjss <frontjss@gmail.com>
2017-06-29 14:59:12 +0300

    row

--- telebot/bot.py
+++ telebot/bot.py
@@ -1,56 +1,21 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import telebot
 import config
 import socket
 import json
-# import cherrypy
-# import config
-
-bot = telebot.TeleBot(config.token)
-
-# a = {"id":"0","name":"durak"}
-
-# sock = socket.socket()
-
-# sock.connect(("172.18.0.108", 9090))
-
 
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start'])
 @bot.message_handler(regexp="Назад")
 def handle_start(message):
-    user_markup = telebot.types.ReplyKeyboardMarkup()
+    user_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     user_markup.row('Получить воду')
     user_markup.row('Пополнить баланс')
     user_markup.row('Статистика')
     user_markup.row('Баланс')
-    # user_markup.row('Получить воду', 'Пополнить баланс')
-    # user_markup.row('Статистика', 'Баланс')
     bot.send_message(message.from_user.id, 'Добро пожаловать', reply_markup=user_markup)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 @bot.message_handler(regexp='Получить воду')
 def handle_start(message):
@@ -58,7 +23,6 @@ def handle_start(message):
     user_markup.row('Назад')
     sent = bot.send_message(message.from_user.id, 'На вашем счету XXX рублей...\nВведите ID водомата', reply_markup=user_markup)
     bot.register_next_step_handler(sent, check)
-
 
 
 @bot.message_handler(regexp='Пополнить баланс')
@@ -88,13 +52,6 @@ def check(message):
     user_markup = telebot.types.ReplyKeyboardMarkup()
     if message.text.isdigit():
         bot.send_message(message.from_user.id, '1 литр 4₽\nПоднесите тару к водомату и нажмите кноку "Старт" на аппарате.', reply_markup=user_markup)
-        # a['id'] = message.text
-        # j = json.dumps(a)
-        
-        # sock.send(j.encode("utf-8"))
-        
-        # data = sock.recv(1024)
-        # print(data)
     elif not (message.text.isdigit()):
         bot.send_message(message.from_user.id, 'Ошибка ввода', reply_markup=user_markup)
         bot.send_message(message.from_user.id, 'Введите ID водомата', reply_markup=user_markup)
