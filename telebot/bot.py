1c8a459f509fc4df4bf7d65cf3ac05772dafe0c9
frontjss <frontjss@gmail.com>
2017-07-04 15:44:41 +0300

    test

--- telebot/bot.py
+++ telebot/bot.py
@@ -22,8 +22,8 @@ def add_user(uid, uname):
     cursor.execute("SELECT id FROM users WHERE id = %i" % (uid))
     results = cursor.fetchall()
     cursor.close()
-    print(uid)
-    print(results)
+    print(uid.id)
+    print(results.id)
     if (str(uid) != str(results)):
         cursor = connection.cursor()
         cursor.execute("INSERT INTO users (id, name) values ( %i, '%s')" % (uid, uname))
