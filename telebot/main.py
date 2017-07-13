5ff22cbd9fd8b41d7f133a4b270590ce5ce839fc
frontjss <frontjss@gmail.com>
2017-07-13 21:41:09 +0300

    cooool

--- telebot/main.py
+++ telebot/main.py
@@ -11,29 +11,17 @@ import pymysql.cursors
 
 # sock.connect(('127.0.0.1', 8080))
 
-
-def connect():
-    connection = pymysql.connect(host='127.0.0.1',
-                                 user='root',
-                                 password='7087',
-                                 db='vodomat',
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
-    return connection
+connection = connection.connect()
 
 
-
-bot = telebot.TeleBot(settings.token)
 
 def add_user(uid, uname):
-    connection = connect()
     cursor = connection.cursor()
     cursor.execute("SELECT idT FROM users WHERE idT = %i" % (uid))
     results = cursor.fetchone()
     cursor.close()
     connection.close()
     if results is None or str(results['idT']) != str(uid):
-        connection = connect()
         cursor = connection.cursor()
         cursor.execute("INSERT INTO users (idT, name, score) values ( %i, '%s', %i)" % (uid, uname, 0))
         connection.commit()   
@@ -42,7 +30,6 @@ def add_user(uid, uname):
         return True
 
 def score(uid):
-    connection = connect()
     cursor = connection.cursor()
     cursor.execute("SELECT score FROM users WHERE idT = %i" % (uid))
     results = cursor.fetchone()
@@ -51,6 +38,8 @@ def score(uid):
     res = results["score"]
     return res
     
+
+bot = telebot.TeleBot(settings.token)
 
 text_welcome = settings.text_welcome
 
