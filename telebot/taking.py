e9301e3af67c5b27699d01cdf68c7eaa46799dd8
frontjss <frontjss@gmail.com>
2017-07-13 23:30:02 +0300

    cooool

--- telebot/taking.py
+++ telebot/taking.py
@@ -7,6 +7,8 @@ bot = telebot.TeleBot(settings.token)
 
 text_id = settings.text_id
 
+text_water = settings.text_water
+
 back_menu_list = settings.back_menu_list
 
 main_menu_list = settings.main_menu_list
@@ -19,13 +21,12 @@ def get_score(message):
 def check(message):
     uid = message.from_user.id
     res = inDB.score(uid)
-    user_markup = telebot.types.ReplyKeyboardMarkup()
     if message.text.isdigit():
         # global infuser
         # infuser['param']['idv'] = int(message.text)
         # infuser['param']['idT'] = message.from_user.id
         # infuser['param']['score'] = res
-        bot.send_message(message.from_user.id, '1 литр 4₽\nПоднесите тару к водомату и нажмите кноку "Старт" на аппарате.', reply_markup=user_markup)
+        bot.send_message(message.from_user.id, text_water, reply_markup=user_markup)
         # j = json.dumps(infuser)
         # sock.send(j.encode("utf-8"))
         # data = sock.recv(2048)
