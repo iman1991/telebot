232e1dc1fcc0f79c338f500d11e40092f64a1d31
frontjss <frontjss@gmail.com>
2017-07-13 22:12:14 +0300

    cooool

--- telebot/inDB.py
+++ telebot/inDB.py
@@ -1,17 +1,15 @@
 import pymysql.cursors
 import connection
 
-
+connect = connection.connect()
 
 def add_user(uid, uname):
-    connection = connection.connect()
     cursor = connect.cursor()
     cursor.execute("SELECT idT FROM users WHERE idT = %i" % (uid))
     results = cursor.fetchone()
     cursor.close()
     connect.close()
     if results is None or str(results['idT']) != str(uid):
-        connect = connection.connect()
         cursor = connect.cursor()
         cursor.execute("INSERT INTO users (idT, name, score) values ( %i, '%s', %i)" % (uid, uname, 0))
         connect.commit()   
@@ -20,7 +18,6 @@ def add_user(uid, uname):
         return True
 
 def score(uid):
-    connect = connection.connect()
     cursor = connect.cursor()
     cursor.execute("SELECT score FROM users WHERE idT = %i" % (uid))
     results = cursor.fetchone()
