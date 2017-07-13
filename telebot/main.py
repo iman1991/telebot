3f9989e6aeeaee62223d93ed4f432706cbfad012
frontjss <frontjss@gmail.com>
2017-07-13 21:59:44 +0300

    cooool

--- telebot/main.py
+++ telebot/main.py
@@ -8,16 +8,18 @@ import socket
 import json
 
 
-connection = connection.connect()
+
 
 
 def add_user(uid, uname):
+    connection = connection.connect()
     cursor = connection.cursor()
     cursor.execute("SELECT idT FROM users WHERE idT = %i" % (uid))
     results = cursor.fetchone()
     cursor.close()
     connection.close()
     if results is None or str(results['idT']) != str(uid):
+        connection = connection.connect()
         cursor = connection.cursor()
         cursor.execute("INSERT INTO users (idT, name, score) values ( %i, '%s', %i)" % (uid, uname, 0))
         connection.commit()   
