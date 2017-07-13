f7ceef3733015239f561853a1e44b26f2b3bd470
frontjss <frontjss@gmail.com>
2017-07-13 16:40:05 +0300

    new

--- telebot/sbot.py
+++ telebot/sbot.py
@@ -92,30 +92,30 @@ def handle_start(message):
 def handle_start(message):
     generator_menu(message, back_menu_list)
 
-def get_water(message):
-    user_markup = telebot.types.ReplyKeyboardMarkup()
-    user_markup.row('Назад')
-    sent = bot.send_message(message.from_user.id, 'Введите ID водомата', reply_markup=user_markup)
-    bot.register_next_step_handler(sent, check)
+    def get_water(message):
+        user_markup = telebot.types.ReplyKeyboardMarkup()
+        user_markup.row('Назад')
+        sent = bot.send_message(message.from_user.id, 'Введите ID водомата', reply_markup=user_markup)
+        bot.register_next_step_handler(sent, check)
 
-def check(message):
-    uid = message.from_user.id
-    uname = message.chat.first_name
-    res = score(uid)
-    user_markup = telebot.types.ReplyKeyboardMarkup()
-    if message.text.isdigit():
-        global infuser
-        infuser['param']['idv'] = int(message.text)
-        infuser['param']['idT'] = message.from_user.id
-        infuser['param']['score'] = res
-        bot.send_message(message.from_user.id, '1 литр 4₽\nПоднесите тару к водомату и нажмите кноку "Старт" на аппарате.', reply_markup=user_markup)
-        j = json.dumps(infuser)
-        sock.send(j.encode("utf-8"))
-        data = sock.recv(2048)
-        print(data)
-    elif not (message.text.isdigit()) and not "Назад":
-        bot.send_message(message.from_user.id, 'Ошибка ввода', reply_markup=user_markup)
-        bot.send_message(message.from_user.id, 'Введите ID водомата', reply_markup=user_markup)
+    def check(message):
+        uid = message.from_user.id
+        uname = message.chat.first_name
+        res = score(uid)
+        user_markup = telebot.types.ReplyKeyboardMarkup()
+        if message.text.isdigit():
+            global infuser
+            infuser['param']['idv'] = int(message.text)
+            infuser['param']['idT'] = message.from_user.id
+            infuser['param']['score'] = res
+            bot.send_message(message.from_user.id, '1 литр 4₽\nПоднесите тару к водомату и нажмите кноку "Старт" на аппарате.', reply_markup=user_markup)
+            j = json.dumps(infuser)
+            sock.send(j.encode("utf-8"))
+            data = sock.recv(2048)
+            print(data)
+        elif not (message.text.isdigit()) and not "Назад":
+            bot.send_message(message.from_user.id, 'Ошибка ввода', reply_markup=user_markup)
+            bot.send_message(message.from_user.id, 'Введите ID водомата', reply_markup=user_markup)
 
 
 
