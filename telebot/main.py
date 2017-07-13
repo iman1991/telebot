48934b94f0346b848f6c7e71121d8f6e85f8efaf
frontjss <frontjss@gmail.com>
2017-07-13 22:47:45 +0300

    cooool

--- telebot/main.py
+++ telebot/main.py
@@ -27,7 +27,9 @@ main_menu_list = settings.main_menu_list
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
+    inDB.add_user(message.from_user.id, message.chat.first_name)
     handlers.answer_text(message, text_welcome, handlers.generator_menu(message, main_menu_list))
+
 
 @bot.message_handler(regexp='Получить воду')
 def handle_start(message):
@@ -36,20 +38,21 @@ def handle_start(message):
     # infuser['method'] = 'GetWater'
     bot.register_next_step_handler(sent, taking.check)
 
+
 @bot.message_handler(regexp='Пополнить баланс')
 def handle_start(message):
     sent = handlers.answer_text(message, text_id, handlers.generator_menu(message, back_menu_list))
     bot.register_next_step_handler(sent, taking.check)
 
+
 @bot.message_handler(regexp='Баланс')
 def handle_start(message):
     handlers.answer_text(message, "{}₽".format(taking.get_score(message)), handlers.generator_menu(message, back_menu_list))
 
+
 @bot.message_handler(regexp='Назад')
 def handle_start(message):
     handlers.answer_text(message, text_get, handlers.generator_menu(message, main_menu_list))
-
-
 
 
 
