3f8447357ec43f3d72ce1d2a26978fb91d33c79b
frontjss <frontjss@gmail.com>
2017-07-13 22:33:45 +0300

    cooool

--- telebot/handlers.py
+++ telebot/handlers.py
@@ -4,7 +4,7 @@ import settings
 bot = telebot.TeleBot(settings.token)
 
 def answer_text(message, answer, user_markup):
-    bot.send_message(message.from_user.id, answer, reply_markup=user_markup)
+    return bot.send_message(message.from_user.id, answer, reply_markup=user_markup)
 
 def generator_menu(message, menu_list):
     user_markup = telebot.types.ReplyKeyboardMarkup()
