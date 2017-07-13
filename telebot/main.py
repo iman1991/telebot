0b7fbf0321a4f22851a21a23eeed6a9f80deb3e4
frontjss <frontjss@gmail.com>
2017-07-13 21:00:00 +0300

    cooool

--- telebot/main.py
+++ telebot/main.py
@@ -14,28 +14,28 @@ bot = telebot.TeleBot(settings.token)
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    answer_text(message, text_welcome, generator_menu(message, main_menu_list))
+    handlers.answer_text(message, text_welcome, generator_menu(message, main_menu_list))
 
 @bot.message_handler(regexp='Получить воду')
 def handle_start(message):
-    answer_text(message, text_id, generator_menu(message, back_menu_list))
+    handlers.answer_text(message, text_id, generator_menu(message, back_menu_list))
     # global infuser
     # infuser['method'] = 'GetWater'
     get_water(message)
 
 @bot.message_handler(regexp='Пополнить баланс')
 def handle_start(message):
-    answer_text(message, text_id, generator_menu(message, back_menu_list))
+    handlers.answer_text(message, text_id, generator_menu(message, back_menu_list))
     add_score(message)
 
 @bot.message_handler(regexp='Баланс')
 def handle_start(message):
-    answer_text(message, get_score(message), generator_menu(message, back_menu_list))
+    handlers.answer_text(message, get_score(message), generator_menu(message, back_menu_list))
     get_score(message)
 
 @bot.message_handler(regexp='Назад')
 def handle_start(message):
-    answer_text(message, text_get, generator_menu(message, main_menu_list))
+    handlers.answer_text(message, text_get, generator_menu(message, main_menu_list))
 
 
 bot.polling(none_stop=True, interval = 0)
