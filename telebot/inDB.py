a8ef0ad30a39067bf873d9e1487a33e83f459e91
frontjss <frontjss@gmail.com>
2017-07-13 22:07:39 +0300

    cooool

--- telebot/inDB.py
+++ telebot/inDB.py
@@ -4,13 +4,14 @@ import connection
 
 
 def add_user(uid, uname):
+    connection = connection.connect()
     cursor = connection.cursor()
     cursor.execute("SELECT idT FROM users WHERE idT = %i" % (uid))
     results = cursor.fetchone()
     cursor.close()
     connection.close()
     if results is None or str(results['idT']) != str(uid):
-        connection = connect()
+        connection = connection.connect()
         cursor = connection.cursor()
         cursor.execute("INSERT INTO users (idT, name, score) values ( %i, '%s', %i)" % (uid, uname, 0))
         connection.commit()   
