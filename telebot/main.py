3f7b0df8dc11da9e6b07af41230b1c6f2a461eb4
frontjss <frontjss@gmail.com>
2017-07-14 15:45:06 +0300

    new

--- telebot/main.py
+++ telebot/main.py
@@ -9,7 +9,6 @@ import socket
 import json
 import gateway
 
-infuser = gateway.infuser
 
 bot = telebot.TeleBot(settings.token)
 
@@ -35,8 +34,7 @@ def handle_start(message):
 
 @bot.message_handler(regexp='Получить воду')
 def handle_start(message):
-    global infuser
-    infuser['method'] = 'GetWater'
+    geteway.infuser.update({'methed':'GetWater'})
     sent = handlers.answer_text(message, text_id, handlers.generator_menu(message, back_menu_list))
     bot.register_next_step_handler(sent, taking.check)
 
@@ -55,8 +53,6 @@ def handle_start(message):
 @bot.message_handler(regexp='Назад')
 def handle_start(message):
     handlers.answer_text(message, text_get, handlers.generator_menu(message, main_menu_list))
-
-
 
 
 bot.polling(none_stop=True, interval = 0)
