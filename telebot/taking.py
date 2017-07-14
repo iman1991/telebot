2449655a56b5f840922e81564b8005d22440a9aa
frontjss <frontjss@gmail.com>
2017-07-14 15:52:51 +0300

    new

--- telebot/taking.py
+++ telebot/taking.py
@@ -39,7 +39,7 @@ def check(message):
         gateway.infuser.update({'param':param})
         j = json.dumps(gateway.infuser)
         sock.send(j.encode("utf-8"))
-        sock.shutdown(socket.RDWR)
+        sock.shutdown(socket.SHUT_RDWR)
         sock.close()
         handlers.answer_text(message, text_water, handlers.generator_menu(message, back_menu_list))
     elif message.text != "Назад":
